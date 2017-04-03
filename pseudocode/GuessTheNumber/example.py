098fa305d94e5f0fc3d186ace7981d199e7f468a
Tom Hanrahan <tom@hanrahan.org.uk>
2017-04-03 10:36:43 +0100

    Initial commit

---
+++ pseudocode/GuessTheNumber/example.py
@@ -0,0 +1,12 @@
+# Tell the user to pick a number between 1 and 10
+# Ask them to press enter when they've done it
+
+# Ask the user yes/no questions about the number until there's only one possible answer left
+
+# When there's only one option left, tell the user their number must be that option.
+
+# Ask them to confirm it.
+
+# Ask the user if they want to play again.
+# If they do, go back to the start
+# If they don't, end the program
